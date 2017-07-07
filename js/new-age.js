07e7647380ea99f113c14437778572706cba2bc8
andytasi <andytasi113@gmail.com>
2017-07-07 19:47:04 +0800

    shrink image size, add GA tracking log and support referer tumblr post

--- js/new-age.js
+++ js/new-age.js
@@ -29,3 +29,55 @@
     })
 
 })(jQuery); // End of use strict
+
+// Replace content if from blog post
+$(document).ready(function() {
+    function getParameterByName(name, url) {
+        if (!url) {
+            url = window.location.href;
+        }
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+        if (!results) {
+            return null;
+        }
+        if (!results[2]) {
+            return '';
+        }
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    var blogName = getParameterByName("blog");
+    var postId = getParameterByName("p");
+
+    if (blogName && postId) {
+        var screenshotDom = $("#screenshot");
+        screenshotDom.attr("src", "./img/screenshot-loading.png");
+
+        $.getJSON('https://api.tumblr.com/v2/blog/' + blogName + '.tumblr.com/posts/photo?callback=?', {
+            api_key: 'wgOM8hf18vYY6j7MgXztXOalk0gZv3ZdHopgYFTWTOUzAhhtb7',
+            id: postId,
+        }).then(function (json) {
+            if (json.response && json.response.posts
+                && json.response.posts.length >= 0 && json.response.posts[0]) {
+
+                var post = json.response.posts[0];
+
+                console.log(post);
+
+                var linkDom = $("#screenshot-link");
+                linkDom.attr("title", post.slug);
+                linkDom.attr("href", post.post_url);
+
+                if (post.photos && post.photos.length > 0 && post.photos[0]) {
+                    var photo = post.photos[0];
+
+                    if (photo.alt_sizes && photo.alt_sizes.length > 0) {
+                        screenshotDom.attr("src", photo.alt_sizes[0].url);
+                    }
+                }
+            }
+        });
+    }
+});
